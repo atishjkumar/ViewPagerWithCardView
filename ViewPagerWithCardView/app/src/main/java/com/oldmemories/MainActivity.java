277ef74b06df1995fac511a548d3b2db10f5ae5e
admin <atish.kumar@redlemondigital.com>
2018-11-02 15:00:22 +0530

    Atish

---
+++ ViewPagerWithCardView/app/src/main/java/com/oldmemories/MainActivity.java
@@ -0,0 +1,34 @@
+package com.oldmemories;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ViewPager mViewPager;
+    private CardPagerAdapter mCardAdapter;
+    private ShadowTransformer mCardShadowTransformer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mCardAdapter = new CardPagerAdapter();
+        mCardAdapter.addCardItem(new CardItem(R.string.title_1, R.string.text_1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_2, R.string.text_1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_3, R.string.text_1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_4, R.string.text_1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_4, R.string.text_1));
+        mCardShadowTransformer = new ShadowTransformer(mViewPager, mCardAdapter);
+        mViewPager.setAdapter(mCardAdapter);
+        mViewPager.setPageTransformer(false, mCardShadowTransformer);
+        mViewPager.setOffscreenPageLimit(3);
+        mViewPager.setCurrentItem(2);
+        mCardShadowTransformer.enableScaling(true);
+    }
+}
